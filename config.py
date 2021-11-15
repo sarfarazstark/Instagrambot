43e786e40eb883138a74b295045c57fc243dfe40
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-15 15:25:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -99,7 +99,6 @@ Example: <code>/followers samantharuthprabhuoffl</code>
 
 This is a bot of [{}](www.instagram.com/{}) to manage his Instagram account. 
 I can only work for my master [{}](tg://user?id={}).
-But you can Deploy the same bot for your use from the below source code.
 
 Use /help to know What I can Do?</b>
 """
