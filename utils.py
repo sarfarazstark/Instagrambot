10b7e9ac33fe5b59aa906d4ac2ac4c022e5b3822
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-15 15:31:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -192,12 +192,12 @@ async def upload(m, bot, chat_id, dir):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/SarfarazStark'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/BlueWhalebots")
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
+
+                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/BlueWhalebots")
 				]
 			]
 			)
