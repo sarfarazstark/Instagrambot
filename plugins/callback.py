cfbfcfc9322f2a7eb23e39c44fe9dc259f79aad0
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-15 15:34:46 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -53,13 +53,9 @@ async def cb_handler(bot: Client, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
 			[
 				[
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122"),
-                    InlineKeyboardButton("⚙️Update Channel", url="https://t.me/subin_works")
-				],
-				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-					InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/SarfarazStark'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/BlueWhalebots"),
+                    InlineKeyboardButton("⚙️Update Channel", url="https://t.me/BlueWhalebots")
 				]
 			]
 			)
